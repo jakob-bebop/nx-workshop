1cf0ad5f3b938b9eb30ffaa2c82dd61722137b1c
Jakob Lund <jakob@avforlaget.dk>
2020-11-18 20:07:44 +0100

    +version

--- tools/schematics/sort-project-references/index.ts
+++ tools/schematics/sort-project-references/index.ts
@@ -1,9 +1,17 @@
 import { chain, externalSchematic, Rule } from '@angular-devkit/schematics';
+import { updateJsonInTree } from '@nrwl/workspace';
 
+function incrementversion() {
+  return updateJsonInTree('package.json', (json) => {
+    const current = json.version.split('.');
+    return {
+      ...json,
+      version: current
+        .map((v, i) => (i !== 2 ? v : String(parseInt(v) + 1)))
+        .join('.'),
+    };
+  });
+}
 export default function (schema: any): Rule {
-  return chain([
-    externalSchematic('@nrwl/workspace', 'lib', {
-      name: schema.name,
-    }),
-  ]);
+  return chain([incrementversion()]);
 }
