1812ea2cbecf65c443ded65c43a73e87db447590
Jakob Lund <jakob@avforlaget.dk>
2020-11-18 18:51:35 +0100

    rules

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+// import { formatRating } from '@bg-hoard/store/util-formatters';
 @Module({
   imports: [],
   controllers: [AppController],
--- libs/store/ui-shared/src/lib/store-ui-shared.module.ts
+++ libs/store/ui-shared/src/lib/store-ui-shared.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
-
+// import { StoreFeatureGameDetailModule } from '@bg-hoard/store/feature-game-detail';
 @NgModule({
   imports: [CommonModule, MatToolbarModule],
   declarations: [HeaderComponent],
