d7eb8f2d0353af5f0261c02259224e055a94eee3
Jakob Lund <jakob@avforlaget.dk>
2020-11-18 21:56:36 +0100

    not quite fixed tests...

--- apps/store/src/app/app.component.spec.ts
+++ apps/store/src/app/app.component.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
-
+import { StoreUiSharedModule } from '@bg-hoard/store/ui-shared';
+import { MatCardModule } from '@angular/material/card';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, StoreUiSharedModule, MatCardModule],
       declarations: [AppComponent],
     }).compileComponents();
   });
